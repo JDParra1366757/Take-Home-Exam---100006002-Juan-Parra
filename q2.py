43e0990f30d685da5567598d70908633ce2fabcd
JDParra1366757 <juandanielparradefaz@gmail.com>
2026-08-10 20:46:51 +0200

    q2 finished

--- q2.py
+++ q2.py
@@ -49,12 +49,77 @@ def takes_all():
         print (f"The student in all classes is: {allclass}")
 
 
-
-
-            
-            
-
+def student_average(student):
+    global grades
+    global subjects
     
+    x = 0
+    num = 0
+    preavg = 0
+    
+    for subjects in grades:
+        for students in grades [subjects]:
+            if students == student:
+                temp = grades[subjects][students]
+                preavg += temp
+                num+=1
+            else:
+                pass
+
+
+    if num > 0:
+        avg = preavg/num
+        print(f"The average for {student} is: {avg:.3}")
+    else:
+        print("Student not enrolled.")
+
+
+
+def honor_roll(grade):
+    global grades
+    global subjects
+    global kids
+
+    num = 0
+    reavg = 0
+    tempi = 0
+    kids=set()
+    honorroll = []
+
+    for subjects in grades:
+        for st in grades[subjects]:
+            kids.add(st) 
+
+    for student in kids:
+        for subjects in grades:
+            for students in grades[subjects]:
+                if students == student:
+                    tempi = grades[subjects][student]
+                    reavg += tempi
+                    num+=1
+                else:
+                    pass
+
+        avg = reavg/num
+
+        if avg <= grade:
+            hr = [student,(round(avg,2))]
+            honorroll.append(hr)
+        else:
+            pass
+
+        avg=0
+        reavg = 0
+        num=0
+        tempi = 0
+
+    honorroll.sort()
+    print(honorroll)
+
+
+            
 
 #subjects_of("anna")
-takes_all()
+#takes_all()
+student_average("anna")
+honor_roll(2)
